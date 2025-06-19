631c05209a660618514c713591fe7b33701142f5
tomellm <thomas.ellmenreich@gmail.com>
2025-06-19 10:49:07 +0200

    initial commit

---
+++ src/app.rs
@@ -0,0 +1,57 @@
+use leptos::prelude::*;
+use leptos_meta::{provide_meta_context, MetaTags, Stylesheet, Title};
+use leptos_router::{
+    components::{Route, Router, Routes},
+    StaticSegment,
+};
+
+pub fn shell(options: LeptosOptions) -> impl IntoView {
+    view! {
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <meta charset="utf-8"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <AutoReload options=options.clone() />
+                <HydrationScripts options/>
+                <MetaTags/>
+            </head>
+            <body>
+                <App/>
+            </body>
+        </html>
+    }
+}
+
+#[component]
+pub fn App() -> impl IntoView {
+    // Provides context that manages stylesheets, titles, meta tags, etc.
+    provide_meta_context();
+
+    view! {
+        // injects a stylesheet into the document <head>
+        // id=leptos means cargo-leptos will hot-reload this stylesheet
+        <Stylesheet id="leptos" href="/pkg/GD_Capital_website.css"/>
+
+        // sets the document title
+        <Title text="Willkommen bei GD-Capital"/>
+
+        // content for this welcome page
+        <Router>
+            <main>
+                <Routes fallback=|| "Seite konnte nicht gefunden werden".into_view()>
+                    <Route path=StaticSegment("") view=HomePage/>
+                </Routes>
+            </main>
+        </Router>
+    }
+}
+
+/// Renders the home page of your application.
+#[component]
+fn HomePage() -> impl IntoView {
+    view! {
+        <h1>"Hallo das ist Fidis website!"</h1>
+        <p>Meine richtig coole beschreibung</p>
+    }
+}
